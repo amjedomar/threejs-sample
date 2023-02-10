0729bdbc7d7741ad900e533ebda7d8259479e24e
Amjed Omar <amjedomar201@gmail.com>
2023-02-10 23:19:50 +0200

    Add depth / jaggedness to plane

--- src/main.ts
+++ src/main.ts
@@ -23,15 +23,27 @@ const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
 const planeMaterial = new THREE.MeshPhongMaterial({
   color: 0xff0000,
   side: THREE.DoubleSide,
+  // @ts-ignore
+  flatShading: THREE.FlatShading
 });
 
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 
 scene.add(planeMesh);
 
-const light = new THREE.DirectionalLight(0xffffff, 1)
-light.position.set(0, 0, 1)
-scene.add(light)
+const arr = (planeMesh.geometry.attributes.position as any).array as number[];
+
+for (let i = 0; i < arr.length; i += 3) {
+  const x = arr[i];
+  const y = arr[i + 1];
+  const z = arr[i + 2];
+
+  arr[i + 2] += Math.random()
+}
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
 
 function animate() {
   requestAnimationFrame(animate);

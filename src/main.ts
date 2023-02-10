97bf56f21d84869a179e66418362a213db1b5e0a
Amjed Omar <amjedomar201@gmail.com>
2023-02-10 22:56:38 +0200

    Add directional lights

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import './style.css'
+import "./style.css";
 
 const scene = new THREE.Scene();
 
@@ -13,25 +13,29 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(innerWidth, innerHeight);
-renderer.setPixelRatio(devicePixelRatio)
+renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
-
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
-const mesh = new THREE.Mesh(boxGeometry, material);
-
-scene.add(mesh);
 
 camera.position.z = 5;
 
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
 
-function animate () {
-  requestAnimationFrame(animate)
+const planeMaterial = new THREE.MeshPhongMaterial({
+  color: 0xff0000,
+  side: THREE.DoubleSide,
+});
+
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+
+scene.add(planeMesh);
+
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(0, 0, 1)
+scene.add(light)
+
+function animate() {
+  requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  mesh.rotation.x += 0.01
-  mesh.rotation.y += 0.01
 }
 
-animate()
+animate();

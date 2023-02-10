b79dc1ff036748c7b354b28bd3ad798696888339
Amjed Omar <amjedomar201@gmail.com>
2023-02-10 22:23:00 +0200

    feat: add a box mesh to the scene

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  innerWidth / innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(innerWidth, innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+const mesh = new THREE.Mesh(boxGeometry, material);
+
+scene.add(mesh);
+
+camera.position.z = 5;
+
+renderer.render(scene, camera);

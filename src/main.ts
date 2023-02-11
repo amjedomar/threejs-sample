26603755d76bed0230996354bafec12169a1d1ba
Amjed Omar <amjedomar201@gmail.com>
2023-02-11 11:01:45 +0200

    Create a sphere

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,34 @@
-import * as THREE from "three";
+import * as THREE from 'three'
+import './style.css'
 
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(
   75,
   innerWidth / innerHeight,
   0.1,
   1000
-);
+)
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(innerWidth, innerHeight)
+document.body.appendChild(renderer.domElement)
 
-renderer.setSize(innerWidth, innerHeight);
-document.body.appendChild(renderer.domElement);
+// create a sphere
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+  })
+)
 
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+scene.add(sphere)
 
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+camera.position.z = 10
 
-const mesh = new THREE.Mesh(boxGeometry, material);
+function animate() {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+}
 
-scene.add(mesh);
-
-camera.position.z = 5;
-
-renderer.render(scene, camera);
+animate()

f58e26e23207bd236e2b5580ad6e42b6296f7a54
Amjed Omar <amjedomar201@gmail.com>
2023-02-11 09:28:18 +0200

    Add hover effect

--- src/main.ts
+++ src/main.ts
@@ -63,21 +63,35 @@ const planeGeometry = new THREE.PlaneGeometry(
 )
 
 const planeMaterial = new THREE.MeshPhongMaterial({
-  color: 0xff0000,
   side: THREE.DoubleSide,
   // @ts-ignore
   flatShading: THREE.FlatShading,
+  vertexColors: true,
 })
 
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
 
+const raycaster = new THREE.Raycaster()
+
 scene.add(planeMesh)
 
-const arr = (planeMesh.geometry.attributes.position as any).array as number[]
+const planeMeshPos = planeMesh.geometry.attributes.position as any
+const arr = planeMeshPos.array as number[]
 
 for (let i = 0; i < arr.length; i += 3) {
   arr[i + 2] += Math.random()
 }
+
+const colors = []
+
+for (let i = 0; i < planeMeshPos.count; i++) {
+  colors.push(1, 0, 0)
+}
+
+planeMesh.geometry.setAttribute(
+  'color',
+  new THREE.BufferAttribute(new Float32Array(colors), 3)
+)
 
 const light = new THREE.DirectionalLight(0xffffff, 1)
 light.position.set(0, 0, 1)
@@ -87,9 +101,45 @@ const backLight = new THREE.DirectionalLight(0xffffff, 1)
 backLight.position.set(0, 0, -1)
 scene.add(backLight)
 
+const mouse = {
+  x: undefined,
+  y: undefined,
+} as any
+
 function animate() {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
+
+  raycaster.setFromCamera(mouse, camera)
+  const intersects = raycaster.intersectObject(planeMesh)
+  if (intersects.length > 0) {
+    const object = intersects[0].object as any
+    const geometry = object.geometry
+    const face = intersects[0].face!
+    const { color } = geometry.attributes
+
+    // vertex 1
+    color.setX(face.a, 0)
+    color.setY(face.a, 0)
+    color.setZ(face.a, 1)
+
+    // vertex 2
+    color.setX(face.b, 0)
+    color.setY(face.b, 0)
+    color.setZ(face.b, 1)
+
+    // vertex 3
+    color.setX(face.c, 0)
+    color.setY(face.c, 0)
+    color.setZ(face.c, 1)
+
+    color.needsUpdate = true
+  }
 }
 
 animate()
+
+window.addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1
+  mouse.y = -(event.clientY / innerHeight) * 2 + 1
+})

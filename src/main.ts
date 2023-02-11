87d0356db189938a89a54f38be4c058a176e60b8
Amjed Omar <amjedomar201@gmail.com>
2023-02-11 12:29:08 +0200

    Add texture to fragment shader cont.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 import './style.css'
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
 
+console.log(vertexShader)
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(
@@ -10,21 +13,30 @@ const camera = new THREE.PerspectiveCamera(
   1000
 )
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+})
 renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
 // create a sphere
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
-  new THREE.MeshBasicMaterial({
-    map: new THREE.TextureLoader().load('./assets/globe.jpg')
+  new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    uniforms: {
+      globeTexture: {
+        value: new THREE.TextureLoader().load('./src/assets/globe.jpg')
+      }
+    }
   })
 )
 
 scene.add(sphere)
 
-camera.position.z = 10
+camera.position.z = 15
 
 function animate() {
   requestAnimationFrame(animate)
